5aa29b9ccb03ee8fb44677641191addf3630487d
LukeM94 <LukeM94@users.noreply.github.com>
2021-02-19 15:11:56 +0000

    Create pingy.py

---
+++ pingy.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+
+print("\033[1m" + "Welcome to the Tech Support Toolbox" + "\033[0m" + " \n Your options are: \n  (1) Ping, \n  (2) IPConfig, \n  (3) GetMac, \n  (4) NSLookup")
+
+def main():
+    option = input("Type an option: ").lower()
+
+    ping = ["1", "ip", "hostname", "ping"]
+    ipconfig = ["2", "ipconfig", "ipconf"]
+    getmac = ["3", "getmac", "mac", "whatmac"]
+    nslookup = ["4", "nslookup", "ns", "lookup"]    
+    
+    if option in ping:
+        pingFunc()
+    elif option in ipconfig:
+        ipconfigFunc()
+    elif option in getmac:
+        getmacFunc()
+    elif option in nslookup:
+        nslookupFunc()
+    else:
+        print("Please type a valid option!")
+
+def pingFunc():
+    ip = input("Enter an IP address or Hostname to ping: ")
+    pingOutput = os.system("ping -n 3 " + ip)
+    if pingOutput == 1:
+        print(ip + " isn't valid")
+    else:
+        print("Ping COMMAND ran successfully. This doesn't mean I pinged anything though!")
+    return pingOutput
+
+def ipconfigFunc():
+    os.system("ipconfig /all")
+
+def getmacFunc():
+    os.system("getmac")
+
+def nslookupFunc():
+    ip = input("Enter an IP address or Hostname to look up: ")
+    os.system("nslookup " + ip)
+
+if __name__ == "__main__":
+    main()
